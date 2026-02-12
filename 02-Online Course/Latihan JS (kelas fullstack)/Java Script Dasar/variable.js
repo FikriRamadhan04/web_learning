07f8c92be0a0f7728c87f675ffdd3d68746d6160
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-13 01:07:58 +0700

    memperbarui nilai number di dala  variabel

--- 02-Online Course/Latihan JS (kelas fullstack)/Java Script Dasar/variable.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/Java Script Dasar/variable.js
@@ -1,4 +1,41 @@
+// deklarasi vairabel
 tahunMerdeka = 1945;
 let selisihTahun = 2026 - tahunMerdeka;
 
 console.log(selisihTahun);
+
+// update (cara 1)
+let score = 0;
+
+score = score + 1;
+score = score + 1;
+score = score + 1;
+score = score + 1;
+
+console.log(score);
+
+// update (cara 2)
+score += 1;
+score += 1;
+score += 1;
+score += 1;
+score += 1;
+score += 1;
+score += 5;
+
+score *= 2;
+score -= 10;
+score /= 2;
+
+score++;
+score++;
+score++;
+score++;
+score++;
+score--;
+score--;
+score--;
+score--;
+score--;
+
+console.log(score);
