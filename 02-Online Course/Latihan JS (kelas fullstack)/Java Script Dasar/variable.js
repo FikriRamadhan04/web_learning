1a234020aceaa5ac0ac0f046655245af9b16a9dc
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-13 23:53:47 +0700

    tips penamaan vairabel

--- 02-Online Course/Latihan JS (kelas fullstack)/Java Script Dasar/variable.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/Java Script Dasar/variable.js
@@ -1,54 +1,64 @@
-// deklarasi vairabel
-tahunMerdeka = 1945;
-let selisihTahun = 2026 - tahunMerdeka;
+// 1. Deklarasi Variabel & Operasi Aritmatika
+const tahunMerdeka = 1945;
+let tahunSekarang = 2026;
+let selisihTahun = tahunSekarang - tahunMerdeka;
 
-console.log(selisihTahun);
+console.log("Selisih Tahun:", selisihTahun); // Output: 81
 
-// update (cara 1)
+// 2. Update Nilai Variabel (score)
 let score = 0;
 
-score = score + 1;
-score = score + 1;
-score = score + 1;
-score = score + 1;
+// Update Cara 1: Manual
+score = score + 1; // score jadi 1
+score = score + 1; // score jadi 2
+console.log("Score Cara 1:", score);
 
-console.log(score);
+// Update Cara 2: Assignment Operators (Lebih singkat)
+score += 10; // Tambah 10
+score *= 2; // Kali 2
+score -= 10; // Kurang 10
+score /= 2; // Bagi 2
+console.log("Score Cara 2:", score);
 
-// update (cara 2)
-score += 1;
-score += 1;
-score += 1;
-score += 1;
-score += 1;
-score += 1;
-score += 5;
+// Update Cara 3: Increment & Decrement (Tambah/Kurang 1)
+score++; // Tambah 1
+score--; // Kurang 1
+console.log("Score Akhir:", score);
 
-score *= 2;
-score -= 10;
-score /= 2;
-
-score++;
-score++;
-score++;
-score++;
-score++;
-score--;
-score--;
-score--;
-score--;
-score--;
-
-console.log(score);
-
-// pake let
+// 3. Perbedaan let dan const
+// Menggunakan let (Bisa di-reassign)
 let ayam = 10;
 ayam = 20;
+console.log("Jumlah ayam:", ayam);
 
-console.log(ayam);
+// Menggunakan const (Tidak bisa di-reassign)
+const tanggalLahir = 19;
+// tanggalLahir = 20; // Ini akan menyebabkan Error!
+console.log("Tanggal lahir:", tanggalLahir);
 
-// pake const (tidak bisa diubah nilanya, akan eror)
-// digunakan untuk nilai pasti yang tidak berubah atau tidak ingin dirubah (contoh:tahun merdeka,tanggal lahir )
-const merdeka = 1945;
-merdeka = 1946;
+// 4. Mengenal Tipe Data Dasar
+let nama = "Budi"; // String (Teks)
+let umur = 25; // Number (Angka)
+let apakahSehat = true; // Boolean (True/False)
+let daftarBelanja = []; // Array (List)
+let dataUser = {}; // Object
 
-console.log(merdeka);
+// 5 Tipe Data Boolean
+let angka = 20;
+
+console.log(angka < 1);
+console.log(angka > 10);
+
+// 6.Tips Memberikan Nama Variabel  (jangan angka,jangan singkat,mudah dipahami sendiri & orang lain)
+
+// Camel Case
+let namaLengkap = "Camel Case";
+
+// Pascal Case
+let Namalengkap = "Pascal Case";
+
+// Snake Case
+let nama_lengkap = "Snake Case";
+
+// simbol
+let $variable = "simbol";
