ad04fea765284a54a2d9d267378c95604a88607c
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-14 19:31:20 +0700

    mengenal string

---
+++ 02-Online Course/Latihan JS (kelas fullstack)/Tipe Data String dan lainnya/script.js
@@ -0,0 +1,12 @@
+// string menggunakan double quote" atau single quote'
+// contoh : "hakim" atau 'hakim'
+// kedua haru sama, jangan double & single di gunakan keduanya
+// string = "20"
+// number = 20
+// studi kasus pengucapan selamat yang ada quote
+// "saya mengucapakan 'selamat'" (pembungkus double quote dan 'selamat' harus single quote) begitu sebaliknya
+
+console.log(20);
+
+console.log("saya mengucapkan 'selamat");
+console.log('saya mengucapkan "selamat"');
