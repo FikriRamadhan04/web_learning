67bb714549f785fddd7e292286d20785b4cdd168
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-17 00:01:32 +0700

    Opeartor pembanding

---
+++ 02-Online Course/Latihan JS (kelas fullstack)/3. JavaScript Logic (Pembuat Keputusan)/script.js
@@ -0,0 +1,14 @@
+//  Operator Pembanding
+console.log(1 > 3);
+console.log(1 < 3);
+console.log(-1 > 1);
+console.log(-1 > -1);
+console.log(-1 <= -1);
+console.log(-1 >= -1);
+
+console.log("a" > "A");
+
+let age = 21;
+
+console.log(age > 6);
+console.log(age > 25);
