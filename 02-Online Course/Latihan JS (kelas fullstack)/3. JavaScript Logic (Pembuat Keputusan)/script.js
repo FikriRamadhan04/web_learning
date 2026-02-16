b69f349151639dc8a295050c1f7276d0278ceec2
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-17 00:21:15 +0700

    perbedaan == dan ===

--- 02-Online Course/Latihan JS (kelas fullstack)/3. JavaScript Logic (Pembuat Keputusan)/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/3. JavaScript Logic (Pembuat Keputusan)/script.js
@@ -1,4 +1,4 @@
-//  Operator Pembanding
+// 1. Operator Pembanding
 console.log(1 > 3);
 console.log(1 < 3);
 console.log(-1 > 1);
@@ -12,3 +12,21 @@ let age = 21;
 
 console.log(age > 6);
 console.log(age > 25);
+
+// 2. Kejanggalan ==
+console.log(5 == 5);
+console.log("b" == "c");
+console.log(7 == "7");
+console.log(0 == "");
+console.log(true == false);
+console.log(0 == false);
+console.log(null == undefined);
+
+// tips
+console.log(5 === 5);
+console.log(1 === 2);
+console.log(7 === "7");
+console.log(0 === false);
+
+console.log(10 != "10");
+console.log(10 !== "10");
