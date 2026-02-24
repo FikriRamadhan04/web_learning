0f0b0b765e0e301c5b60239f1007345abb439a52
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-25 00:02:00 +0700

    Mengenal Struktur Data Array

---
+++ 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Array/script.js
@@ -0,0 +1,12 @@
+// 1. mengenal Struktur Data Array
+let movie = [
+  "6 underground",
+  "the dark tower",
+  "jujutsu kaisen",
+  "Extraction",
+  "The Gray Man",
+];
+
+let angka = [1, 2, 3, 4, 5, 6];
+
+let campuran = ["laptop", 1, true, null];
