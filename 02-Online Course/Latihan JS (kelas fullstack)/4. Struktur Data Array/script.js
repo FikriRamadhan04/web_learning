b7716e6dfdcee2dc22d3dcd7c42e0bb8167e69ec
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-02-26 23:54:21 +0700

    Mendapatkan Nilai Dengan Index dan Mengubah Isinya

--- 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Array/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Array/script.js
@@ -1,12 +1,27 @@
 // 1. mengenal Struktur Data Array
-let movie = [
-  "6 underground",
-  "the dark tower",
-  "jujutsu kaisen",
-  "Extraction",
-  "The Gray Man",
-];
+// let movie = [
+//   "6 underground",
+//   "the dark tower",
+//   "jujutsu kaisen",
+//   "Extraction",
+//   "The Gray Man",
+// ];
 
-let angka = [1, 2, 3, 4, 5, 6];
+// let angka = [1, 2, 3, 4, 5, 6];
 
-let campuran = ["laptop", 1, true, null];
+// let campuran = ["laptop", 1, true, null];
+
+// 2. Mendapatkan Nilai Dengan Index Dan Mengubah Isinya
+let warna = ["hijau", "kuning", "kelab", "bu"];
+
+warna[2] = "kelabu";
+warna[3] = "merah muda";
+warna[0] = 1;
+warna[9] = "biru";
+warna[4] = "hijau";
+
+console.log(warna[2]);
+console.log(warna[3]);
+console.log(warna[0]);
+console.log(warna[9]);
+console.log(warna);
