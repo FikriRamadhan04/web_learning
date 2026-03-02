2219961be676227f79880fdc83a47d2c55118b02
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-03-02 23:51:03 +0700

    spice

--- 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Array/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Array/script.js
@@ -1,30 +1,30 @@
 // 1. mengenal Struktur Data Array
-// let movie = [
-//   "6 underground",
-//   "the dark tower",
-//   "jujutsu kaisen",
-//   "Extraction",
-//   "The Gray Man",
-// ];
+let movie = [
+  "6 underground",
+  "the dark tower",
+  "jujutsu kaisen",
+  "Extraction",
+  "The Gray Man",
+];
 
-// let angka = [1, 2, 3, 4, 5, 6];
+let angka = [1, 2, 3, 4, 5, 6];
 
-// let campuran = ["laptop", 1, true, null];
+let campuran = ["laptop", 1, true, null];
 
 // 2. Mendapatkan Nilai Dengan Index Dan Mengubah Isinya
-// let warna = ["hijau", "kuning", "kelab", "bu"];
+let warna = ["hijau", "kuning", "kelab", "bu"];
 
-// warna[2] = "kelabu";
-// warna[3] = "merah muda";
-// warna[0] = 1;
-// warna[9] = "biru";
-// warna[4] = "hijau";
+warna[2] = "kelabu";
+warna[3] = "merah muda";
+warna[0] = 1;
+warna[9] = "biru";
+warna[4] = "hijau";
 
-// console.log(warna[2]);
-// console.log(warna[3]);
-// console.log(warna[0]);
-// console.log(warna[9]);
-// console.log(warna);
+console.log(warna[2]);
+console.log(warna[3]);
+console.log(warna[0]);
+console.log(warna[9]);
+console.log(warna);
 
 // 3.Method Array Push & Pop
 
@@ -47,13 +47,26 @@ barbel.pop();
 
 console.log(barbel);
 
+// 4.Method Array Unshift Dan Shift
+
 //unshift menambahkan item pada pada awal array
 let student = ["Berry", "Charlie ", "Densen", "Edwin"];
 
 student.unshift("Andrew");
 student.unshift("Alex");
 
+console.log(student);
+
 //shift menghapus item dari paling awal array
 student.shift();
 
 console.log(student);
+
+// 5.Splice
+const months = ["Jan", "march", "April", "Jun"];
+
+months.splice(1, 0, "feb");
+console.log(months);
+
+months.splice(4, 1, "May");
+console.log(months);
