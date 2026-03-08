57b0897812e54bf1bab4d44a02ab7cd5b59413b7
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-03-09 00:07:53 +0700

    cara membuat struktur data object

--- 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Object/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/4. Struktur Data Object/script.js
@@ -1,6 +1,25 @@
-// Contoh Data Object
+// 1. Contoh Data Object(key:value)
 const fitnessData = {
   totalSteps: 1341234,
   totalMiles: 119.2,
   avgCaloriesburn: 6654,
+};
+
+// 2. Cara Membuat Struktur Data Object
+// struktur data object dapat menampung berbagai tipe data sekaligus
+const orang = {
+  nama: "Andrew",
+  tanggal: 21,
+  alamat: "Jakarta",
+};
+
+const barang = {
+  judul: "Sepatu",
+  harga: 2000000,
+  isReady: true,
+  size: [38, 29, 40, 41, 42],
+  location: {
+    indonesia: 48,
+    singapore: 50,
+  },
 };
