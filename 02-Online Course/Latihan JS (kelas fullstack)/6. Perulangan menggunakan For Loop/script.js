a54f57486b07dab754ba577cd6953fda96803f7c
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-03-15 23:12:52 +0700

    contoh lain perulangan for

--- 02-Online Course/Latihan JS (kelas fullstack)/6. Perulangan menggunakan For Loop/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/6. Perulangan menggunakan For Loop/script.js
@@ -13,5 +13,19 @@
 // console.log(10);
 
 for (let i = 1; i <= 10; i++) {
+  console.log("saya ada di dalam loop");
+  console.log(i);
+}
+
+// 2. Contoh Lain perulangan For
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+for (let i = 20; i >= 0; i--) {
+  console.log(i);
+}
+
+for (let i = 10; i <= 100000; i *= 10) {
   console.log(i);
 }
