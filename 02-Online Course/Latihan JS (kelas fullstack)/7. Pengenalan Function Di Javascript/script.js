f010b67e8d4dddd1b7b193994238af4669266dbb
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-04-15 08:52:20 +0700

    Pengenalan Function Di Javascipt

---
+++ 02-Online Course/Latihan JS (kelas fullstack)/7. Pengenalan Function Di Javascript/script.js
@@ -0,0 +1,10 @@
+// 1. Pengenalan Function Di Javascipt
+
+// let LemparDadu = Math.floor(Math.random() * 6) + 1;
+// console.log(LemparDadu);
+
+// lemparDadu = console.log(lemparDadu);
+
+function lemparDadu() {
+  console.log(Math.floor(Math.random() * 6) + 1);
+}
