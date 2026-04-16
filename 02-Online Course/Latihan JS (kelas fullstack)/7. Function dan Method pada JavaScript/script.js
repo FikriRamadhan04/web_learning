bec04f5d9fbe3a858b18814569835ddb559a4adc
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-04-16 08:21:41 +0700

    Argument dan parameter dalam Function

---
+++ 02-Online Course/Latihan JS (kelas fullstack)/7. Function dan Method pada JavaScript/script.js
@@ -0,0 +1,44 @@
+// 1. Pengenalan Function Di Javascipt
+
+// let LemparDadu = Math.floor(Math.random() * 6) + 1;
+// console.log(LemparDadu);
+
+// lemparDadu = console.log(lemparDadu);
+
+// function lemparDadu() {
+//   console.log(Math.floor(Math.random() * 6) + 1);
+// }
+
+// 2. Mendefinisikan Dan Menjalankan Function
+// function nyanyi() {
+//   console.log("Cek...");
+//   console.log("Do RE MI...");
+// }
+// nyanyi();
+
+function absensi() {
+  console.log("Absen 1");
+  console.log("absen 2");
+  console.log("absen 3");
+}
+
+absensi();
+
+// 3. Argument Dan Parameter Dalam Function
+
+// name = Parameter;
+// Jason = Argument;
+
+// Contoh 1
+function greet(name) {
+  console.log(`Hi How Are You, ${name} ?`);
+}
+
+greet("Jason");
+
+// Contoh 2
+function selamatPagi(name) {
+  console.log(`Hai, Selamat pagi ${name}   ?`);
+}
+
+selamatPagi("Kane");
