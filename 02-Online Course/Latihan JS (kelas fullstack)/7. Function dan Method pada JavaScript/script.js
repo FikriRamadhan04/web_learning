87db6543f332f5529161e8dfd2487017c8f1559c
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-04-26 09:04:53 +0700

    Definisi Sebuah Method

--- 02-Online Course/Latihan JS (kelas fullstack)/7. Function dan Method pada JavaScript/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/7. Function dan Method pada JavaScript/script.js
@@ -124,46 +124,62 @@
 //   membeli();
 // }
 
-// 9. Function Expressions
+// // 9. Function Expressions
 // function perpangkatan(nilai) {
 //   return nilai * nilai;
 // }
 
 // let hasil = perpangkatan(5);
 
-// Function Expressions
+// // Function Expressions
 // const hasilPerpangkatan = function (nilai, pembanding) {
 //   return nilai * pembanding;
 // };
 
-// 10. Function Sebagai Argument Function Lain
-function enamKali(func) {
-  func();
-  func();
-  func();
-  func();
-  func();
-  func();
+// // 10. Function Sebagai Argument Function Lain
+// function enamKali(func) {
+//   func();
+//   func();
+//   func();
+//   func();
+//   func();
+//   func();
+// }
+
+// function lemparDadu() {
+//   const hasil = Math.floor(Math.random() * 6) + 1;
+//   console.log(hasil);
+// }
+
+// // 11. Function Bernilai Balik Function
+// function hasilnyaAdalahFunction() {
+//   const rand = Math.random();
+//   if (rand > 0.1) {
+//     return function () {
+//       console.log("Selamat, angkanya lebih besar");
+//     };
+//   } else {
+//     return function () {
+//       console.log("Maaf , mungkin bisa coba lagi");
+//     };
+//   }
+// }
+// const result = hasilnyaAdalahFunction();
+// result();
+// hasil();
+
+// 12. Definisi Sebuah Method
+
+function myFun() {
+  // Function biasa
+  console.log("Hi");
+  return "Hello";
 }
 
-function lemparDadu() {
-  const hasil = Math.floor(Math.random() * 6) + 1;
-  console.log(hasil);
-}
-
-// 11. Function Bernilai Balik Function
-function hasilnyaAdalahFunction() {
-  const rand = Math.random();
-  if (rand > 0.1) {
-    return function () {
-      console.log("Selamat, angkanya lebih besar");
-    };
-  } else {
-    return function () {
-      console.log("Maaf , mungkin bisa coba lagi");
-    };
-  }
-}
-const result = hasilnyaAdalahFunction();
-result();
-hasil();
+const myMath = {
+  // Object
+  perkalian: function (x, y) {
+    // Method
+    return x * y;
+  },
+};
