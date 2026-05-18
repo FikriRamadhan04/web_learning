e0091fea3d2988489470bb34aedc062ffaf6b318
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-05-18 23:48:58 +0700

    arrow function

--- 02-Online Course/Latihan JS (kelas fullstack)/8. Callback Function dan Method-method milik Array/script.js
+++ 02-Online Course/Latihan JS (kelas fullstack)/8. Callback Function dan Method-method milik Array/script.js
@@ -19,22 +19,22 @@ angka.forEach(function (antrian) {
 // Misal Pemenang Undian
 
 // deklarasi nilai array, misal pemenang undiannya (misal ada 1.000 lebih)
-// const peserta = [
-//   24, 34, 52, 465, 24, 227, 45, 56, 78, 43, 45, 7956, 402, 929.1394,
-// ];
+const peserta = [
+  24, 34, 52, 465, 24, 227, 45, 56, 78, 43, 45, 7956, 402, 929.1394,
+];
 
 // buat daftar pemenang dulu
-// const daftarPemenang = [24, 34, 929, 56, 45];
+const daftarPemenang = [24, 34, 929, 56, 45];
 
 // gunakan metode .forEach() untuk mengecek data array
-// peserta.forEach(function (nomor) {
-// keputusan da function untuk menentukan pemenang
-//   if (daftarPemenang.includes(nomor)) {
-//     console.log(`nomor ${nomor} kamu menang`);
-//   } else {
-//     console.log(`nomor ${nomor} kamu belum beruntung`);
-//   }
-// });
+peserta.forEach(function (nomor) {
+  // keputusan da function untuk menentukan pemenang
+  if (daftarPemenang.includes(nomor)) {
+    console.log(`nomor ${nomor} kamu menang`);
+  } else {
+    console.log(`nomor ${nomor} kamu belum beruntung`);
+  }
+});
 
 // contoh 2 (menampikan daftar barang)
 const keranjangBelanja = ["sepatu", "buku", "sepeda"];
@@ -47,15 +47,25 @@ keranjangBelanja.forEach((barang, index) => {
 // Kamu punya daftar nama, lalu kamu ingin mengubah semuanya jadi huruf kapital untuk disimpan di daftar baru.
 
 // - Inisialisasi Array
-// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // - Proses Mapping
 // - const angkaDouble: Menyiapkan variabel baru untuk menampung hasil akhir dari proses map.
 // - angka.map(...): Memanggil metode .map() pada array angka.
 // - function (num): Ini adalah callback function. JavaScript akan menjalankan fungsi ini sebanyak 9 kali (sesuai jumlah isi array). Setiap kali jalan, satu angka dari array dimasukkan ke dalam parameter num.
 // - return num * 2: Inilah instruksi perubahannya. Setiap angka yang masuk (num) akan dikali 2, lalu hasilnya "dikembalikan" untuk disusun ke dalam array baru.
-// const angkaDouble = angka.map(function (num) {
-//   return num * 2;
-// });
-// console.log(angka);
-// console.log(angkaDouble);
+const angkaDouble = angka.map(function (num) {
+  return num * 2;
+});
+console.log(angka);
+console.log(angkaDouble);
+
+// 3. Arrow Function
+
+// const perpangkatan = (x) => {
+//   return x * x;
+// };
+
+const random = () => {
+  return Math.floor(Math.random() * 1000);
+};
