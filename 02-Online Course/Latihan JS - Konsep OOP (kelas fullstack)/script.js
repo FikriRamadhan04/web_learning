41ffa3f140d07824f9ad5d8afe76f06cddd15137
fikri ramadhan <fikriramadhan1152@gmail.com>
2026-07-26 10:10:30 +0700

    Membuat Object Dengan Factory Function

--- 02-Online Course/Latihan JS - Konsep OOP (kelas fullstack)/script.js
+++ 02-Online Course/Latihan JS - Konsep OOP (kelas fullstack)/script.js
@@ -20,4 +20,38 @@ class Kucing {
 // - Buat Objek Kucing
 const kucingPertama = new Kucing("Milo", "Oranye");
 console.log(kucingPertama.perkenalkan());
+0;
 console.log(kucingPertama.bersuara());
+
+// 2. Membuat Object Dengan Factory Function
+// Factory Function adalah sebuah fungsi di JavaScript yang bertugas untuk membuat dan mengembalikan (return) sebuah object.
+// Secara sederhana, bayangkan fungsi ini seperti sebuah "pabrik" (factory): Anda memasukkan bahan-bahan (parameter), dan pabrik tersebut akan merakit serta mengeluarkan sebuah object jadi yang siap digunakan.
+// - Mendefinisikan Factory Function
+function buatMobil(merk, model, tahun) {
+  return {
+    merk: merk,
+    model: model,
+    tahun: tahun,
+
+    //- Method di dalam object
+    infoMobil: function () {
+      return `Mobil ini adalah ${this.merk} ${this.model} keluaran tahun ${this.tahun}.`;
+    },
+
+    //- Method untuk menyalakan mesin
+    nyalakanMesin: function () {
+      console.log(`Mesin ${this.merk} ${this.model} berhasil dinyalakan! 🚗💨`);
+    },
+  };
+}
+
+// - Membuat beberapa object menggunakan Factory Function
+const mobil1 = buatMobil("Toyota", "Avanza", 2022);
+const mobil2 = buatMobil("Honda", "Civic", 2023);
+
+// - Menggunakan object yang telah dibuat
+console.log(mobil1.merk); // Output: Toyota
+console.log(mobil2.infoMobil()); // Output: Mobil ini adalah Honda Civic keluaran tahun 2023.
+
+mobil1.nyalakanMesin();
+// - Output: Mesin Toyota Avanza berhasil dinyalakan!
