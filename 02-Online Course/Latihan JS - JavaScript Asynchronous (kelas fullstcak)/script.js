640b98d7e523bfde2b99514a8caac2905dd2b2ae
Fikri Ramadhan <fikriramadhan1152@gmail.com>
2026-08-09 23:53:03 +0700

    Function yang mensimulasikan proses asynchronous (misal: mengambil data dari server)

--- 02-Online Course/Latihan JS - JavaScript Asynchronous (kelas fullstcak)/script.js
+++ 02-Online Course/Latihan JS - JavaScript Asynchronous (kelas fullstcak)/script.js
@@ -10,3 +10,26 @@ console.log("Kedua: Proses lain berjalan duluan");
 // Pertama: Mulai proses
 // Kedua: Proses lain berjalan duluan
 // Ketiga: Proses asynchronous selesai (setelah 2 detik)
+
+// Function yang mensimulasikan proses asynchronous (misal: mengambil data dari server)
+function ambilDataDariServer() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data berhasil diterima dari server!");
+    }, 2000); // Penundaan selama 2 detik (2000 ms)
+  });
+}
+
+// Function async untuk menjalankan proses secara asynchronous
+async function prosesUtama() {
+  console.log("1. Mulai mengambil data...");
+
+  // Menunggu (await) sampai fungsi ambilDataDariServer selesai
+  const hasil = await ambilDataDariServer();
+  console.log("2.", hasil);
+
+  console.log("3. Proses selesai!");
+}
+
+// Jalankan fungsi
+prosesUtama();
